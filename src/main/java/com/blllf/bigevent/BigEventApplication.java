918cc8c1b61cf5d752211de101d97f59fb06fd78
longfei <longfei@qq.com>
2024-05-11 10:46:20 +0800

    second commit

--- src/main/java/com/blllf/bigevent/BigEventApplication.java
+++ src/main/java/com/blllf/bigevent/BigEventApplication.java
@@ -8,6 +8,7 @@ public class BigEventApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BigEventApplication.class, args);
+        System.out.println("hello world");
     }
 
 }
